01e14fae07d6ee4a6c9151b0d5364395ebd1761b
Vaibhav Sharma <vsham31@gmail.com>
2025-08-19 18:51:20 +0000

    renamed tsx to ts for correct file extention & usage within the scope of file

--- lib/set-root-theme.ts
+++ lib/set-root-theme.ts
@@ -1,6 +1,5 @@
 export function setRootTheme(theme: 'light' | 'dark') {
   const body = document.body;
-//   const root = document.documentElement || document.querySelector('.root');
   if (theme === 'dark') {
     body.style.background = '#0b0b0b';
     body.style.color = '#fafafa';
